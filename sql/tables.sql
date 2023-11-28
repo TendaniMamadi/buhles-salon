ef7158cc919e5b3c0826240335dab527a116c33b
TendaniMamadi <mamadi.tendani@gmail.com>
2023-11-28 14:32:47 +0200

    tables created

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,1 +1,40 @@
 -- Table create scripts here
+
+-- Client table
+CREATE TABLE client (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    phone_number VARCHAR(20),
+    treatment VARCHAR(255)
+);
+
+-- Stylist table
+CREATE TABLE stylist (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    phone_number VARCHAR(20),
+    commission_percentage NUMERIC(3,2)
+);
+
+-- Treatment table
+CREATE TABLE treatment (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(255),
+    code VARCHAR(3),
+    price NUMERIC(10,2)
+);
+
+-- Booking table
+CREATE TABLE booking (
+    id SERIAL PRIMARY KEY,
+    booking_date DATE,
+    booking_time TIME,
+    client_id INT,
+    treatment_id INT,
+    stylist_id INT,
+    FOREIGN KEY (client_id) REFERENCES client(id),
+    FOREIGN KEY (treatment_id) REFERENCES treatment(id),
+    FOREIGN KEY (stylist_id) REFERENCES stylist(id)
+);
